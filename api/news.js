c735ea288820d8e6a3636df4f4f491a641363791
DevMan77Code <devman.an77@gmail.com>
2026-03-02 18:22:13 +0530

    fix pagination issue

--- api/news.js
+++ api/news.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      `https://newsapi.org/v2/everything?q=${q}&pageSize=10&apiKey=${process.env.NEWS_API_KEY}`
+      `https://newsapi.org/v2/everything?q=${q}&pageSize=100&apiKey=${process.env.NEWS_API_KEY}`
     );
 
     const data = await response.json();
